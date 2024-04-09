d0b194b315889c16bdc7cc17af21a61468ca0b66
DanBuii23 <danbuii2001@gmail.com>
2024-04-09 17:39:36 +0700

    init 2

---
+++ PKX_DATN/Controllers/TimKiemController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PKX_DATN.Models;
+
+namespace PKX_DATN.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        private readonly WebPkxContext _context;
+        public TimKiemController(WebPkxContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FindProduct(string keyword)
+        {
+            List<TblSanPham> ls = new List<TblSanPham>();
+            if (string.IsNullOrEmpty(keyword) || keyword.Length < 1)
+            {
+                return PartialView("ListProductsSearchPartial", null);
+            }
+            ls = _context.TblSanPhams
+                .AsNoTracking()
+                .Include(a => a.IdDanhMuc)
+                .Where(x => x.STenSanPham.Contains(keyword))
+                .OrderByDescending(x => x.STenSanPham)
+                .Take(10)
+                .ToList();
+            if (ls == null)
+            {
+                return PartialView("ListProductsSearchPartial", null);
+            }
+            else
+            {
+                return PartialView("ListProductsSearchPartial", ls);
+            }
+        }
+    }
+}
