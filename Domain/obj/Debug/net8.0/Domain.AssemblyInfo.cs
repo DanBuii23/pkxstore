34a65678e736ded613855b52e8d48292298176dd
Thuan <mthuan222@gmail.com>
2024-04-30 19:01:53 +0700

    Change datasource

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d280dc6ea74a9e3da5c14a0d77c21c0eca023ac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b6c3fee2fda6608613e34b7d1ca036c78c479cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
